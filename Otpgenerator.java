36949e7fb7ae8b88a6b27909f5515d96fee0ebe3
Adisanap3136 <154910450+Adisanap3136@users.noreply.github.com>
2025-02-13 01:46:58 +0530

    Otp validation

---
+++ Otpgenerator.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+class Otpgenerator
+{
+	public static void main(String[] args)
+	{
+		String otp="";
+		for(int i=1;i<=1;)
+		{
+			otp += (int)(Math.random()*1000000);
+			if(otp.length()==6)
+			{
+				i++;
+				System.out.println(otp);
+			}
+			System.out.print("Enter the otp: ");
+			String op=new Scanner(System.in).nextLine();
+			if (otp.equals(op))
+			{
+				System.out.println("Login Successful!");
+			}
+			else
+			{
+				System.out.println("Invalid otp");
+			}
+		}
+	}
+}
